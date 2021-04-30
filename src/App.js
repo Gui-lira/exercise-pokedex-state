ff306b651840aa8685676bd4a0e1f872133abe86
Gui-lira <gui.lira.martins@gmail.com>
2021-04-29 23:08:51 -0300

    exercicio e parte do bonus

--- src/App.js
+++ src/App.js
@@ -1,15 +1,40 @@
-import React from 'react';
+import React, {Component} from 'react';
 import './App.css';
 import pokemons from './data';
 import Pokedex from './Pokedex';
 
-function App() {
-  return (
-    <div className="App">
-      <h1> Pokedex </h1>
-      <Pokedex pokemons={pokemons} />
-    </div>
-  );
+
+class App extends Component {
+  constructor() {
+    super()
+    this.filterclick = this.filterclick.bind(this);
+    this.state = {
+      buttonValue: 'Normal'
+    }
+  }
+  reducingArr(array) {
+    const arr = [];
+    array.forEach(element => {
+      if (!arr.includes(element.type)) {
+        arr.push(element.type)
+      }
+    });
+    // console.log(arr);
+    return arr;
+  }
+  filterclick(event) {
+    const test = event.target.value
+    this.setState((_props) => ({ buttonValue: test }))
+  }
+  render() {
+    return (
+      <div className="App">
+        <h1> Pokedex </h1>
+        <Pokedex pokemons={pokemons.filter((pokemon) => pokemon.type === this.state.buttonValue)} />
+        {this.reducingArr(pokemons).map((type) => <button onClick={this.filterclick} value={type}>{type}</button>)}
+      </div>
+    );
+  }  
 }
 
 export default App;
--- src/Pokedex.js
+++ src/Pokedex.js
@@ -2,10 +2,33 @@ import React from 'react';
 import Pokemon from './Pokemon';
 
 class Pokedex extends React.Component {
+    constructor() {
+        super()
+        this.NextPokemon = this.NextPokemon.bind(this)
+        this.state = {
+            index: 0
+        }
+    }
+    NextPokemon() {
+        const length = this.props.pokemons.length;
+        if (this.state.index === length - 1) {
+            this.setState({ index: 0})
+        }
+        else {
+            this.setState((pastState,_props) => ({
+                index: pastState.index + 1
+            }))
+        }
+    }
     render() {
+        // console.log(this.state)
+        const pokemon = this.props.pokemons[this.state.index]
+        // console.log(pokemon);
         return (
+            
             <div className="pokedex">
-                {this.props.pokemons.map(pokemon => <Pokemon key={pokemon.id} pokemon={pokemon} />)}
+                <Pokemon key={pokemon.id} pokemon={pokemon} />
+                <button onClick={this.NextPokemon}>Next</button>
             </div>
         );
     }
